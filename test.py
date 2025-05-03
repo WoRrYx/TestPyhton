e17885efe631f555023e6a26b1f42bf86116e693
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 11:45:34 +0200

    update some function

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-print ("hello wprld ")
-print ("Mohamed")
-print ("hello mohamed ")
+print ("hello ")
+print ("My name is Mohamed")
+print ("my age 23 ")
+print ("Student at faculty of infromation tecnology")
 a =8 +2 * 10 
-print (a)
+print ("a = " , a)
+b = (8+2)*10
+print ("b = ", b) 
