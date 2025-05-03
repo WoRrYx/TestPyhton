c7fb121700bbe1ebe8495d96e722ce021ed9d69d
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 15:46:17 +0200

    for loop

--- test.py
+++ test.py
@@ -116,3 +116,21 @@ elif name is "any one":
     print ("go away")
 else:
     print ("please enter right name hehe")
+
+
+# for loop
+foods = ["tuna" , 'fish' , 'meat' , 'chicken'  ]
+for f in foods :
+    print (f)
+    print (len(f))
+
+print ("")
+for x in range (10):
+    print (x)
+
+
+for i in range (5):
+     print("you'r grate you'r the best")
+
+for i in range (10 , 55 , 5):
+    print(i)
