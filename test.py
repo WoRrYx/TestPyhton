7a086d02b5e5a9e3e1378d442bf41a7ea1dfd3b8
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 11:08:51 +0200

    update title & variables

--- test.py
+++ test.py
@@ -1,8 +1,38 @@
+#title
 print ("hello ")
 print ("My name is Mohamed")
 print ("my age 23 ")
 print ("Student at faculty of infromation tecnology")
+
+#numbers 
 a =8 +2 * 10 
 print ("a = " , a)
+
 b = (8+2)*10
 print ("b = ", b) 
+
+c = 18/4 
+print ("c = ", c)
+
+c1 = 18 //4 
+print ("c1 = ", c1)
+
+c2 = 18%4 
+print ("c2 = ", c2)
+
+d = 5*5*5
+print ("d = " , d)
+
+d1 = 5**3
+print ("d1 = " , d1) 
+
+#variables
+rocky = 5 
+print ("rocky = " , 5 + rocky )
+
+baila = 20 
+baila + rocky
+print ("BailaRocky " , baila + rocky)
+
+#string 
+
