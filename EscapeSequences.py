1c174a324c056f2dfda93c09dc65ed9aa792335f
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 15:28:54 +0200

    Escape Sequences

---
+++ EscapeSequences.py
@@ -0,0 +1,10 @@
+# Escape Sequences characters
+# \b => back spaec
+# \  newline => Escape New line + \
+# \\ => Escape back Slash
+# \' => Escape single qoute
+# \" => Escape double qoutes
+# \n => new line
+# \r => carriage return
+# \t => horizontal tab 
+# \xhh => character with hex value hh
