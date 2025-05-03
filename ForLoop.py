f6b1b4882f3ea307870c52d9be70582b6014360e
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 15:29:13 +0200

    For Loop

---
+++ ForLoop.py
@@ -0,0 +1,16 @@
+# for loop
+foods = ["tuna" , 'fish' , 'meat' , 'chicken'  ]
+for f in foods :
+    print (f)
+    print (len(f))
+
+print ("")
+for x in range (10):
+    print (x)
+
+
+for i in range (5):
+     print("you'r grate you'r the best")
+
+for i in range (10 , 55 , 5):
+    print(i)
