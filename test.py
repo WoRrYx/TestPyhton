cc34c4a926483f45bb7eccb52e33aed989254880
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 11:11:58 +0200

    if statment

--- test.py
+++ test.py
@@ -93,5 +93,26 @@ print (players[2])
 print (players)
 
 #اضافة عناصر في نهاية المصفوفة  
+# تعني افعل شي ما للمصفوفة .
+# append اضافة
 players.append(99)
 print(players)
+
+#if statemnt
+
+age = 23
+if age <= 20 :
+    print ("you can't buy this bequse you ander 20")
+elif age >= 20 :
+    print ("you can buy yhis")
+
+# if statemnt in string
+
+name = "proten"
+
+if name is "proten":
+    print ("i love you proten")
+elif name is "any one":
+    print ("go away")
+else:
+    print ("please enter right name hehe")
