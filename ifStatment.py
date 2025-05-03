515ef93427a397f88e0ad20559864189f21001fe
Mohamed <mohamedalzway09@gmail.com>
2025-05-03 15:17:37 +0200

    if statment

---
+++ ifStatment.py
@@ -0,0 +1,18 @@
+#if statemnt
+
+age = 23
+if age <= 20 :
+    print ("you can't buy this bequse you ander 20")
+elif age >= 20 :
+    print ("you can buy yhis")
+
+# if statemnt in string
+
+name = "proten"
+
+if name is "proten":
+    print ("i love you proten")
+elif name is "any one":
+    print ("go away")
+else:
+    print ("please enter right name hehe")
